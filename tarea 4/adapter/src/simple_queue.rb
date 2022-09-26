08d019325dab490a8499a33bbe5dc3c9c50b0580
ZabdielV <premiunn@hotmail.com>
2022-09-26 18:09:10 -0500

    mas documentacion

--- tarea 4/adapter/src/simple_queue.rb
+++ tarea 4/adapter/src/simple_queue.rb
@@ -7,17 +7,21 @@
 
 # IMPORTANT: Do not modify the following class in any way!
 
+#A class that implements First-In First-Out (FIFO) data structure
 class SimpleQueue
 
+    #initializes an empty array
     def initialize
       @info =[]
     end
-  
+
+    #Inserts x at the back of this queue. Returns this queue.
     def insert(x)
       @info.push(x)
       self
     end
   
+    #Removes and returns the element at the front of this queue. Raises an exception if this queue happens to be empty.
     def remove
       if empty?
         raise "Can't remove if queue is empty"
@@ -26,14 +30,17 @@ class SimpleQueue
       end
     end
   
+    #Returns true if this queue is empty, otherwise returns false.
     def empty?
       @info.empty?
     end
   
+    #Returns the number of elements currently stored in this queue.
     def size
       @info.size
     end
   
+    #Prints the content of the current queue structure.
     def inspect
       @info.inspect
     end
