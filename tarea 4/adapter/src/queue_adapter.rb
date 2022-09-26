4052f2496301138deae160932920730c3a733a8d
ZabdielV <premiunn@hotmail.com>
2022-09-26 17:30:17 -0500

    clase documentada

--- tarea 4/adapter/src/queue_adapter.rb
+++ tarea 4/adapter/src/queue_adapter.rb
@@ -6,19 +6,22 @@
 #          A01377072 Emiliano Heredia García
 
 
-
+#This class allows simple_queue to act as Last-In First-Out (LIFO) stack
 class QueueAdapter
   attr_reader :queue
 
+  #Initializes a queue as attribute to act as stack.
     def initialize(q)
       @queue=q
     end
-
+    
+    #Inserts x at the top of this stack. Returns this stack.
     def push(x)
       @queue.insert(x)
       self
     end
 
+    #Returns nil if this stack is empty, otherwise removes and returns its top element.
     def pop
       if @queue.empty?
         nil
@@ -31,6 +34,8 @@ class QueueAdapter
       end
     end
 
+
+    #Returns nil if this stack is empty, otherwise returns its top element without removing it.
     def peek
       if @queue.empty?
         nil
@@ -40,15 +45,17 @@ class QueueAdapter
         element
       end
     end
-
+    #Returns true if this stack is empty, otherwise returns false.
     def empty?
       @queue.empty?
     end
 
+    #Returns the number of elements currently stored in this stack.
     def size
       @queue.size
     end
     
+    #Prints the content of the current stack structure.
     def inspect
       @queue.inspect
     end
