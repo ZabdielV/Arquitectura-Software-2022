8a23897d4ba0bd4e414a57261c2c6f69d2a23eac
ZabdielV <A01377950@itesm.mx>
2022-10-09 10:57:39 -0500

    tarea 6 archivos

---
+++ tarea 6/dsl/src/jankenpon.rb
@@ -0,0 +1,7 @@
+# Domain-Specific Language Pattern
+# Date: 10-Oct-2022
+# Authors:
+#          A01377950 Zabdiel Valentín Garduño Vivanco
+#          A01377942 Luis Jonathan Rosas Ramos
+#          A01377072 Emiliano Heredia García
+
